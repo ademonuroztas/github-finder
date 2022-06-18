cd7cb5888181083ca88d3c60a3665bcbe87f2522
ademonuroztas <ademonuroztas@gmail.com>
2022-06-18 11:26:29 +0300

    Demonstrate same output without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
   render() {
-    return (
-      <React.Fragment>
-        <h1>Hello from React</h1>
-        <h2>Goodbye</h2>
-      </React.Fragment>
+    return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement('h1', null, 'Hello from React')
     );
   }
 }
