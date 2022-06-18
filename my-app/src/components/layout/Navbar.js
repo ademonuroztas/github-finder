972ad5d3a2d65b8c9dba0e20624f0c0d1d0675b9
ademonuroztas <ademonuroztas@gmail.com>
2022-06-18 14:57:41 +0300

    Getting Started With Component State

--- my-app/src/components/layout/Navbar.js
+++ my-app/src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
   static defaultProps = {
@@ -9,14 +9,16 @@ export class Navbar extends Component {
 
   static propTypes = {
     title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
+    icon: PropTypes.string.isRequired,
   };
 
   render() {
     return (
-      <h1>
-        <i className={this.props.icon}></i> {this.props.title}
-      </h1>
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon}></i> {this.props.title}
+        </h1>
+      </nav>
     );
   }
 }
